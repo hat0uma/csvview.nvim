a166c2849bbf862bd2d4e2286e7b4c983d3775dd
rikuma-t <55551571+rikuma-t@users.noreply.github.com>
2023-11-07 01:11:37 +0900

    refactor(config): Add comments

--- lua/csvview/config.lua
+++ lua/csvview/config.lua
@@ -3,10 +3,15 @@ local M = {}
 --- @class CsvViewOptions
 M.defaults = {
   parser = {
+    --- The number of lines that the asynchronous parser processes per cycle.
+    --- This setting is used to prevent monopolization of the main thread when displaying large files.
+    --- If the UI freezes, try reducing this value.
     async_chunksize = 50,
   },
   view = {
+    --- minimum width of a column
     min_column_width = 5,
+    --- spacing between columns
     spacing = 2,
   },
 }
