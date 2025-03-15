fcef2e670d70e028544911927aab9fa6c5453346
hat0uma <55551571+hat0uma@users.noreply.github.com>
2025-03-15 21:21:42 +0900

    test: add testcase for multi-character delimiter

--- tests/util_spec.lua
+++ tests/util_spec.lua
@@ -3,148 +3,158 @@ local csvview = require("csvview")
 local testutil = require("tests.testutil")
 local util = require("csvview.util")
 
-local opts = {
-  parser = {
-    delimiter = ",",
-    quote_char = '"',
-    comments = { "#" },
-  },
-  view = {},
-}
-
-local lines = {
-  "# Comment line",
-  "field1",
-  "",
-  "field2,field3,field4",
-  "🥰abc,a😊c,abc😎",
-}
-
-local cases = {
-  {
-    name = "should return comment cursor",
-    cases = {
-      {
-        name = "at the start of the comment",
-        cursor = { 1, 0 },
-        expected = { kind = "comment", pos = { 1 } },
-      },
-      {
-        name = "at the end of the comment",
-        cursor = { 1, 14 },
-        expected = { kind = "comment", pos = { 1 } },
-      },
+--- Create test cases
+---@param delimiter string
+---@return CsvView.Options, string[], { name:string, cases:{ name:string, cursor:integer[],expected:CsvView.Cursor}[] }[]
+local function create_test(delimiter)
+  local opts = {
+    parser = {
+      delimiter = delimiter,
+      quote_char = '"',
+      comments = { "#" },
     },
-  },
-  {
-    name = "should return field cursor",
-    cases = {
-      {
-        name = "at the start of the field",
-        cursor = { 2, 0 },
-        expected = {
-          kind = "field",
-          pos = { 2, 1 },
-          anchor = "start",
-          text = "field1",
+    view = {},
+  }
+
+  local lines = {
+    "# Comment line",
+    "field1",
+    "",
+    table.concat({ "field2", "field3", "field4" }, delimiter),
+    table.concat({ "🥰abc", "a😊c", "abc😎" }, delimiter),
+  }
+
+  local cases = {
+    {
+      name = "should return comment cursor",
+      cases = {
+        {
+          name = "at the start of the comment",
+          cursor = { 1, 0 },
+          expected = { kind = "comment", pos = { 1 } },
         },
-      },
-      {
-        name = "inside the field",
-        cursor = { 2, 4 },
-        expected = {
-          kind = "field",
-          pos = { 2, 1 },
-          anchor = "inside",
-          text = "field1",
-        },
-      },
-      {
-        name = "at the end of the field",
-        cursor = { 2, 5 },
-        expected = {
-          kind = "field",
-          pos = { 2, 1 },
-          anchor = "end",
-          text = "field1",
-        },
-      },
-      {
-        name = "at the delimiter",
-        cursor = { 4, 6 },
-        expected = {
-          kind = "field",
-          pos = { 4, 1 },
-          anchor = "delimiter",
-          text = "field2",
+        {
+          name = "at the end of the comment",
+          cursor = { 1, #lines[1] - 1 },
+          expected = { kind = "comment", pos = { 1 } },
         },
       },
     },
-  },
-  {
-    name = "should return empty line cursor",
-    cases = {
-      {
-        name = "",
-        cursor = { 3, 0 },
-        expected = {
-          kind = "empty_line",
-          pos = { 3 },
+    {
+      name = "should return field cursor",
+      cases = {
+        {
+          name = "at the start of the field",
+          cursor = { 2, 0 },
+          expected = {
+            kind = "field",
+            pos = { 2, 1 },
+            anchor = "start",
+            text = "field1",
+          },
+        },
+        {
+          name = "inside the field",
+          cursor = { 2, #lines[2] - 2 },
+          expected = {
+            kind = "field",
+            pos = { 2, 1 },
+            anchor = "inside",
+            text = "field1",
+          },
+        },
+        {
+          name = "at the end of the field",
+          cursor = { 2, #lines[2] - 1 },
+          expected = {
+            kind = "field",
+            pos = { 2, 1 },
+            anchor = "end",
+            text = "field1",
+          },
+        },
+        {
+          name = "at the delimiter",
+          cursor = { 4, string.len("field2") + #delimiter - 1 },
+          expected = {
+            kind = "field",
+            pos = { 4, 1 },
+            anchor = "delimiter",
+            text = "field2",
+          },
         },
       },
     },
-  },
-  {
-    name = "should return field cursor with multibyte characters",
-    cases = {
-      {
-        name = "at the start of the field",
-        cursor = { 5, 0 },
-        expected = {
-          kind = "field",
-          pos = { 5, 1 },
-          anchor = "start",
-          text = "🥰abc",
-        },
-      },
-      {
-        name = "inside the field",
-        cursor = { 5, string.len("🥰abc,a😊") - 1 },
-        expected = {
-          kind = "field",
-          pos = { 5, 2 },
-          anchor = "inside",
-          text = "a😊c",
-        },
-      },
-      {
-        name = "at the end of the field",
-        cursor = { 5, string.len("🥰abc,a😊c,abc😎") - 1 },
-        expected = {
-          kind = "field",
-          pos = { 5, 3 },
-          anchor = "end",
-          text = "abc😎",
-        },
-      },
-      {
-        name = "at the delimiter",
-        cursor = { 5, string.len("🥰abc,") - 1 },
-        expected = {
-          kind = "field",
-          pos = { 5, 1 },
-          anchor = "delimiter",
-          text = "🥰abc",
+    {
+      name = "should return empty line cursor",
+      cases = {
+        {
+          name = "",
+          cursor = { 3, 0 },
+          expected = {
+            kind = "empty_line",
+            pos = { 3 },
+          },
         },
       },
     },
-  },
-}
+    {
+      name = "should return field cursor with multibyte characters",
+      cases = {
+        {
+          name = "at the start of the field",
+          cursor = { 5, 0 },
+          expected = {
+            kind = "field",
+            pos = { 5, 1 },
+            anchor = "start",
+            text = "🥰abc",
+          },
+        },
+        {
+          name = "inside the field",
+          cursor = { 5, string.len(string.format("🥰abc%sa😊", delimiter)) - 2 },
+          expected = {
+            kind = "field",
+            pos = { 5, 2 },
+            anchor = "inside",
+            text = "a😊c",
+          },
+        },
+        {
+          name = "at the end of the field",
+          cursor = { 5, string.len(string.format("🥰abc%sa😊c%sabc😎", delimiter, delimiter)) - 1 },
+          expected = {
+            kind = "field",
+            pos = { 5, 3 },
+            anchor = "end",
+            text = "abc😎",
+          },
+        },
+        {
+          name = "at the delimiter",
+          cursor = { 5, string.len("🥰abc" .. delimiter) - 1 },
+          expected = {
+            kind = "field",
+            pos = { 5, 1 },
+            anchor = "delimiter",
+            text = "🥰abc",
+          },
+        },
+      },
+    },
+  }
 
-describe("util", function()
-  csvview.setup(opts)
+  return opts, lines, cases
+end
 
-  describe("get_cursor", function()
+--- Run test cases
+--- @param opts CsvView.Options
+--- @param lines string[]
+--- @param cases { name:string, cases:{ name:string, cursor:integer[],expected:CsvView.Cursor}[] }[]
+local function run(opts, lines, cases)
+  describe(string.format("get_cursor (delimiter=%s)", opts.parser.delimiter), function()
+    csvview.setup(opts)
     -- create buffer and set lines
     local bufnr = vim.api.nvim_create_buf(false, true)
     vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
@@ -172,4 +182,9 @@ describe("util", function()
       end)
     end
   end)
+end
+
+describe("util", function()
+  run(create_test(","))
+  run(create_test("|||||"))
 end)
